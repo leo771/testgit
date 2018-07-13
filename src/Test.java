feab10d8ffba0229e7f1c57f397824545f2e1d0e
qiulin.wang <qiulin.wang@luckincoffee.com>
2018-07-13 11:17:08 +0800

    commit2

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2018/7/13.
+ */
+public class Test {
+
+    public  static void main(String[] args){
+        String a = "0";
+        System.out.print("dsfafdadf");
+
+    }
+}
